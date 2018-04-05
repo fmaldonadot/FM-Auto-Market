63cbdc8274aa327ee19e8557f69520cf39dad8ed
fmaldonadot <35792576+fmaldonadot@users.noreply.github.com>
2018-04-05 19:00:39 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+
+<title>Index</title>
+</head>
+
+<body style="background-color: beige">
+  <h1 sytle: align="center">FMAuto Market</h1>
+  <p sytle: align="center">&nbsp;</p>
+</body>
+</html>
